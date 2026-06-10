1aebb280b3dce8c65386fcaf1752fe4517e25dfb
Seán Stratton <t-sstratton@microsoft.com>
2026-06-10 15:19:03 +0100

    fix permission issues

--- components/copy_kvp_client.sh
+++ components/copy_kvp_client.sh
@@ -2,20 +2,24 @@
 set -e
 
 DEST_DIR=/opt/azurehpc/tools
-mkdir -p $DEST_DIR
+sudo mkdir -p $DEST_DIR
 
-# Download the file
+# Ensure gcc is available
+sudo apt-get update
+sudo apt-get install -y gcc
+
+# Download source
 wget https://raw.githubusercontent.com/microsoft/lis-test/master/WS2012R2/lisa/tools/KVP/kvp_client.c
 
-# ✅ Fix outdated code
+# Fix outdated code
 
 # Fix missing return type for main()
 sed -i 's/^main(/int main(/' kvp_client.c
 
-# Disable kvp_key_exists usage (prevents compile error)
+# Disable kvp_key_exists usage
 sed -i 's/kvp_key_exists([^)]*)/0/g' kvp_client.c
 
 # Move and compile
-mv ./kvp_client.c $DEST_DIR
-
-gcc $DEST_DIR/kvp_client.c -o $DEST_DIR/kvp_client
+sudo mv kvp_client.c $DEST_DIR
+sudo gcc $DEST_DIR/kvp_client.c -o $DEST_DIR/kvp_client
+``
